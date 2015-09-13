f6025d4a31e62369bc957fc221c2c4b4a0e7f458
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-13 13:23:42 +0700

    update pagination

--- Resource-Authorization-Server/routes/UserRestRoute.js
+++ Resource-Authorization-Server/routes/UserRestRoute.js
@@ -8,12 +8,15 @@ var router = express.Router();
 router.get('/user', passport.authenticate('accessToken', {
   session: false
 }), function(req, res) {
-  User.find({}, function(err, users) {
-    if (err) return res.json(err);
 
-    res.json(users);
+  User.find({}).skip((req.query.page - 1) * req.query.jumlah)
+    .limit(req.query.jumlah)
+    .exec(function(err, users) {
+      if (err) return res.json(err);
 
-  });
+      res.json(users);
+    });
+
 });
 
 module.exports = router;
