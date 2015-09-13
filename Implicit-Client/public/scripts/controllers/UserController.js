d16441364d58b37fbacc28e20208da224976a340
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-13 13:46:55 +0700

    update pagination angular

--- Implicit-Client/public/scripts/controllers/UserController.js
+++ Implicit-Client/public/scripts/controllers/UserController.js
@@ -9,14 +9,37 @@
     var user = this;
 
     user.dataUser = {};
+    user.paging = {
+      page: 1,
+      jumlah: 5
+    };
 
     function getUser() {
-      UserFactory.getUsers($crypto.decrypt($window.sessionStorage.getItem('token'))).query({}, function(data) {
-        user.dataUser = data;
+      UserFactory.getUsers($crypto.decrypt($window.sessionStorage.getItem('token')), user.paging.page, user.paging.jumlah).query({}, function(data) {
+        user.dataUser = data.documents;
+
+        user.paging.totalPages = data.totalPages;
+
       });
     }
 
     getUser();
+
+    //paging
+    user.nextPage = function() {
+      if (user.paging.page < user.paging.totalPages) {
+        user.paging.page++;
+        getUser();
+      }
+    };
+
+    user.previousPage = function() {
+      if (user.paging.page > 1) {
+        user.paging.page--;
+        getUser();
+      }
+    };
+    //end paging
 
   }
 
