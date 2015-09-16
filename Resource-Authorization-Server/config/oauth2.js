e37375a30589e196c463853be2d75a8999afca86
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-16 12:18:55 +0700

    update oauth2

--- Resource-Authorization-Server/config/oauth2.js
+++ Resource-Authorization-Server/config/oauth2.js
@@ -4,6 +4,7 @@ var oauth2orize = require('oauth2orize');
 var passport = require('passport');
 var crypto = require('crypto');
 var bcrypt = require('bcrypt');
+var _ = require('underscore')._;
 var Client = require('../models/Client');
 var AccessToken = require('../models/AccessToken');
 var uuid = require('./uuid');
@@ -35,24 +36,53 @@ server.deserializeClient(function(id, done) {
 
 //Implicit grant
 server.grant(oauth2orize.grant.token(function(client, user, ares, done) {
+
   var token = uuid.uid(256);
   var tokenHash = encrypt(token);
-  var tokenExpired = new Date(new Date().getTime() + (3600 * 1000))
+  var tokenExpired = new Date(new Date().getTime() + (3600 * 1000));
 
-  var accessToken = new AccessToken({
-    token: token,
-    tokenExpired: tokenExpired,
-    userId: user.username,
-    clientId: client.clienId
+  var clientId = {};
+
+  _.each(client, function(c) {
+    clientId = c.clientId;
   });
 
-  accessToken.save(function(err) {
-    if (err) return done(err)
-    return done(null, token, {
-      expires_in: tokenExpired.toISOString(),
-      access_token: tokenHash
-    });
+  AccessToken.findOne({
+    clientId: clientId
+  }, function(err, accessToken) {
+    if (err) return done(err);
+
+    if (accessToken) {
+      accessToken.token = token;
+      accessToken.tokenExpired = tokenExpired;
+      accessToken.save();
+
+      return done(null, token, {
+        expires_in: tokenExpired.toISOString(),
+        access_token: tokenHash
+      });
+
+    } else {
+
+      console.log(clientId);
+
+      var accessToken = new AccessToken({
+        token: token,
+        tokenExpired: tokenExpired,
+        userId: user.username,
+        clientId: clientId
+      });
+
+      accessToken.save(function(err) {
+        if (err) return done(err)
+        return done(null, token, {
+          expires_in: tokenExpired.toISOString(),
+          access_token: tokenHash
+        });
+      });
+    }
   });
+
 }));
 
 // user authorization endpoint
