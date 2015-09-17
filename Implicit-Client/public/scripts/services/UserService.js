464124a42a857c5fcac6d568a2dcf159afe100cd
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-09-17 23:20:48 +0700

    add url

--- Implicit-Client/public/scripts/services/UserService.js
+++ Implicit-Client/public/scripts/services/UserService.js
@@ -10,6 +10,9 @@
     userService.getUsers = function(page, jumlah) {
       return 'http://localhost:3000/api/user?page=' + page + '&jumlah=' + jumlah;
     };
+    userService.deleteUser = function(id) {
+      return 'http://localhost:3000/api/user/' + id;
+    };
 
     return userService;
 
